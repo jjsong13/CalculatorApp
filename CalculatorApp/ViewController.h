15dd7ffe098ef5dddd7aac1bdbd2c846309de527
Jamie <jsj.jamie@gmail.com>
2016-07-11 14:31:34 +0900

    Set up ViewController interface

--- CalculatorApp/ViewController.h
+++ CalculatorApp/ViewController.h
@@ -8,8 +8,35 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+//Declare enumeration of current status
+typedef enum {
+    STATUS_DEFAULT=0,
+    STATUS_DIVISION,
+    STATUS_MULTIPLY,
+    STATUS_MINUS,
+    STATUS_PLUS,
+    STATUS_RETURN
+}  kStatusCode;
 
+@interface ViewController : UIViewController {
+    double curValue; //property to save currently inputted value
+    double totalCurValue; //property to save final calculated value
+    NSString *curInputValue; //property to save currrent input numbers
+    kStatusCode curStatusCode;
+}
+
+//method to take care of event when number button is clicked
+- (IBAction) digitPressed:(UIButton *) sender;
+
+//method to take care of event when function button is clicked
+- (IBAction) operationPressed:(UIButton *)sender;
+
+@property Float64 curValue; //declare current input value as property
+@property Float64 totalCurValue; //declare total calculated value as property
+@property kStatusCode curStatusCode; //declare current status as property
+
+//declare an outlet for display label
+@property (weak,nonatomic) IBOutlet UILabel *displayLabel;
 
 @end
 
