005f1638a49fb592677232943a172ca467cc244d
Jamie <jsj.jamie@gmail.com>
2016-07-11 13:47:55 +0900

    Initial Commit

---
+++ CalculatorApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorApp
+//
+//  Created by Jamie Jyhyun Song on 7/11/16.
+//  Copyright (c) 2016 JSONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorApp
+//
+//  Created by Jamie Jyhyun Song on 7/11/16.
+//  Copyright (c) 2016 JSONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
